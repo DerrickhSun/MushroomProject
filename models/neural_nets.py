54d24682250cad53305cf9df354bde8c9b2d0e6c
DerrickhSun <derrick.han.sun@gmail.com>
2025-04-06 16:26:38 -0400

    mostly done nn, needs debugging

---
+++ models/neural_nets.py
@@ -0,0 +1,106 @@
+import csv
+import os
+import torch
+from torch import nn
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("mushroom_dataset_enum.csv")
+print(df.shape)
+
+features = [col for col in df.columns if col != 'class']
+discarded = []
+
+class neural_network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embeddings = []
+
+        count = 0
+        for feature in features:
+            n_categories = df[feature].nunique()
+            embed_dim = min(50, (n_categories + 1) // 2)
+            count+=embed_dim
+
+            self.embeddings.append(nn.Embedding(n_categories, embed_dim))
+
+        self.model = nn.Sequential(
+            nn.Linear(count, 500),
+            nn.ReLU(),
+            nn.Linear(500, 500),
+            nn.ReLU(),
+            nn.Linear(500, 1),
+            nn.Softmax(dim=1)
+        )
+        
+
+    def forward(self, x):
+        embed_inputs = []
+        for feature_index in range(len(features)):
+            embed = self.embeddings[feature_index](x[:,feature_index])
+            embed_inputs.append(embed)
+            #print(embed.shape)
+        x = torch.cat(embed_inputs, dim = 1)
+        #print(x.shape)
+        return self.model(x).squeeze(1)
+
+
+# takes in dataframe for data
+def train(model, optimizer, data, loss_funct, batches = 1000, batch_size = 2000):
+
+    for epoch in range(2):
+        shuffled = data.sample(frac = 1)
+        x = torch.from_numpy(shuffled[features].values).int()
+        y = torch.from_numpy(shuffled["class"].values).float()
+        running_loss = 0.0
+        for i in range(min(batches, int(data.shape[0]/batch_size))):#range(data.shape[0]):
+            inputs = x[batch_size*i : batch_size*(i+1)]
+            labels = y[batch_size*i : batch_size*(i+1)]
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = loss_funct(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            running_loss += loss.item()
+            #if i % 10 == 9:    # print every 10 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss/10 :.5f}')
+            running_loss = 0.0
+
+
+def evaluate(model, data, loss_funct):
+    x = torch.from_numpy(data[features].values).float()
+    y = torch.from_numpy(data["class"].values).float()
+    pred = model(x)
+    return loss_funct(pred, y)
+
+
+
+model = neural_network()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.00001, momentum=0.9)
+
+train(model, optimizer, df, nn.MSELoss())
+torch.save(model.state_dict(), "nn_MSE")
+
+print(df[features].shape)
+
+x = df[features]
+y = df["class"]
+#print(x.dtypes)
+
+'''models = []
+for i in range(1, 10, 1):
+    percentile = 0.1*i
+    model = quantile_regression()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.00001, momentum=0.9)
+
+    
+    train(model, optimizer, df, quantile_loss())
+    models.append(model)
+    torch.save(model.state_dict(), "quantile_regression"+str(int(percentile*100))+"percentile")
+    print("saved percentile regression " + str(percentile))'''
